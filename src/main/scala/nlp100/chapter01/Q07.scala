46e6107dd821ed8f44825d9d1ab22066bd6e5daa
pecorarista <pecorarista@gmail.com>
2015-11-03 03:40:25 +0900

    Fix Q07

--- src/main/scala/nlp100/chapter01/Q07.scala
+++ src/main/scala/nlp100/chapter01/Q07.scala
@@ -1,8 +1,7 @@
 package nlp100.chapter01
 
-object Q07 extends App {
+object Q07 {
 
-  def template(x: Int, y: String, z: Double): Unit =
-    println(x.toString + "時の" + y + "は" + z.toString)
+  def template(x: Int, y: String, z: Double): Unit = println(s"${x}時の${y}は${z}")
 
 }
