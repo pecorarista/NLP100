7d0dc0d52c3651a5710f3103626e7bf052c82e34
pecorarista <pecorarista@gmail.com>
2015-11-04 05:27:10 +0900

    Add Q13

---
+++ src/main/scala/nlp100/chapter02/Q13.scala
@@ -0,0 +1,17 @@
+package nlp100.chapter02
+
+import java.io.PrintWriter
+import scala.io.Source
+
+object Q13 extends App {
+
+  val p = new PrintWriter("src/main/resources/merged.txt")
+
+  val col1 = Source.fromURL(getClass.getResource("/col1.txt")).getLines.toList
+  val col2 = Source.fromURL(getClass.getResource("/col2.txt")).getLines.toList
+
+  col1.zip(col2).foreach({ case (x, y) =>  p.println(s"${x}\t${y}") })
+
+  p.close
+
+}
